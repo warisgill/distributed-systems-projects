42c694d942030859fce0abaec8faddac9d6566f3
Waris Gill <wgill18@ku.edu.tr>
2019-02-24 19:24:37 +0300

    first version

---
+++ A1/client.py
@@ -0,0 +1,148 @@
+import sys
+import socket
+import threading
+
+
+class Client:
+    def __init__(self, server_ip, port):
+        """
+        This is the constructor of the client class. 
+        
+        Args:
+            server_ip: The IP of the server which you want to connect. 
+            port:  Port Number of the server 
+        """
+        self.server_ip = server_ip
+        self.server_port = port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.buffsize = 1024
+        self.timestamp = 0
+        self.quit = False
+        self.username = ""
+        self.lock_timestamp = threading.Lock()
+        self.lock_quit = threading.Lock()
+        self.threads = []
+
+    def connect(self):
+        """ 
+            This method is used to connect to the server. It also create a new thread for user input
+            and handle all events on client side. 
+        """   
+        print("Local Timestamp: ", self.timestamp)
+        self.timestamp += 1
+        self.client_socket.connect((self.server_ip, self.server_port))
+
+        username = input("Please enter your username: ")
+        buffer = "{username},<{timestamp}>".format(
+            username=username, timestamp=self.timestamp)
+
+        self.timestamp += 1
+        self.client_socket.sendall(bytes(buffer, "utf-8"))
+
+        while True:
+            buffer = self.client_socket.recv(self.buffsize)
+            if len(buffer) == 0:
+                print(">> Handle Server Exit.")
+                self.client_socket.close()
+                exit()
+
+            buffer = buffer.decode('utf-8')
+            #print("\n>>Check Quit", buffer)
+            ts = self.getTimeStamp(buffer)
+            self.incrementTimeStamp(ts)
+            message = buffer.split(",")[0]
+
+            with self.lock_quit:
+                if self.quit:
+                    break
+            if message == "Accepted":
+                self.username = username
+                t = threading.Thread(target=self.sendMessage, args=())
+                t.start()
+                self.threads.append(t)
+                print("\nAccepted.\n")
+            elif message == "Rejected":
+
+                print("Rejected.")
+                username = input(
+                    "\nYour username has already been taken, enter a new one: ")
+                self.incrementTimeStamp(self.timestamp)
+                buffer = "{username},<{timestamp}>".format(
+                    username=username, timestamp=self.timestamp)
+                self.client_socket.sendall(bytes(buffer, "utf-8"))
+
+            elif message == "quit":
+                #print("Check: here you go.")
+                #self.client_socket.sendall(bytes("11", 'utf-8'))
+                print("Exit Done.\n")
+                break
+            else:
+                print("\n{message},<{timestamp}>\n".format(
+                    message=message, timestamp=self.timestamp))
+
+        self.client_socket.close()
+        print("Exiting receiver thread.")
+
+    def sendMessage(self):
+        """
+        This message is passed to the thread which is created in the connect() and 
+        it handle the user interaction
+        """   
+        while True:
+            buffer = input("",)
+            check = buffer
+            self.incrementTimeStamp(self.timestamp)
+            buffer = "{message},<{ts}>".format(
+                message=buffer, ts=self.timestamp)
+            self.client_socket.sendall(bytes(buffer, 'utf-8'))
+
+            if check == self.username + " quit":
+                with self.lock_quit:
+                    self.quit = True
+                break
+        print("Exiting sender thread.")
+
+    def incrementTimeStamp(self, timestamp):
+        """
+        This method is used to increment the timestamp according to lamport's algorithm.
+        
+        Args:    
+            timestamp: timestamp to comapare.
+        
+        Returns: 
+            updated timestamp 
+        """   
+        temp = 0
+        
+        with self.lock_timestamp:
+            self.timestamp = max(self.timestamp, timestamp) + 1
+            temp = self.timestamp
+
+        return temp
+
+    def getTimeStamp(self, message):
+        
+        """
+        This method will extract the timestamp from message and returns it as integer.
+        Args:
+            message: Its the message of type string.
+        
+        Returns: 
+            It will return the timestamp of type int.
+        """   
+        message = message.split('<')[1]
+        message = message.split('>')[0]
+        return int(message)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Please rerun the program and enter the IP of server.\n")
+        exit()
+
+    IP = sys.argv[1]
+    PORT = 4444
+
+    client = Client(IP, PORT)
+    client.connect()
---
+++ A1/server.py
@@ -0,0 +1,198 @@
+import socket
+import threading
+import time
+
+
+class Server:
+    def __init__(self, ip, port):
+        """
+        It's the constructor of the server class. 
+        Args:    
+           ip:  IP of the server
+           port: Port of the server at which it will start listening
+             
+        """
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.ip, self.port))
+
+        self.timestamp = 0
+        self.buffsize = 1024
+        self.clients = {}
+        self.threads = []
+        self.lock_clients = threading.Lock()
+        self.lock_timestamp = threading.Lock()
+
+    def listen(self):
+        """
+            This method will handle incoming connection and will create a new thread for each
+            incoming connection.     
+        """
+        print(">> Local Timestamp: ", self.timestamp)
+        try:
+            print(">> Server is listening at port 4444.")
+            self.socket.listen(4)  # backlog 4 clients at same time
+            #self.timestamp += 1
+            while True:
+                client_socket, client_addr = self.socket.accept()
+                self.incrementTimeStamp(self.timestamp)
+                # client_socket.timeout(60)
+                print(">> Client connected.")
+                args_tuple = (client_socket, client_addr)
+                t = threading.Thread(
+                    target=self.handleNewClient, args=args_tuple)
+                t.start()
+                self.threads.append(t)
+        except KeyboardInterrupt:
+            print("Server Closed.")
+            self.socket.close()
+            exit()
+
+    def handleNewClient(self, client_socket, client_addr):
+        """
+        This method is passed to the new thread created on each incoming connection and it will
+        with all kind of client server interaction.
+        
+        Args:    
+            client_socket: It's the client socket which is just connected.
+            client_addr: It's the tuple which contains client connection info.   
+             
+        """
+        flag, username = self.registerNewUser(client_socket)
+        quit_condition = "{u} quit".format(u=username)
+
+        if flag:
+            while True:
+                buffer = client_socket.recv(self.buffsize)
+
+                if len(buffer) == 0:
+                    print(">> Force Exit.")
+                    with self.lock_clients:
+                        self.clients.pop(username, None)
+                    buffer = "{u} left the messenger room, <{ts}>".format(
+                        u=username, ts=self.timestamp)
+                    self.broadcast(username, buffer)
+                    break
+
+                buffer = buffer.decode('utf-8')
+                message = buffer.split(',')[0]
+                ts = self.getTimeStamp(buffer)
+                self.incrementTimeStamp(ts)
+
+                if message == quit_condition:
+                    self.handleQuit(username)
+                    break
+
+                buffer = "{u}: {message}, <{timestamp}>".format(
+                    u=username, message=message, timestamp=self.timestamp)
+                self.broadcast(username, buffer)
+
+    def registerNewUser(self, client_socket):
+        """
+            This method handles all the interactions of new client registeratins.
+        Args:    
+            client_socket: Client socket who wants to register. 
+        
+        Returns: 
+            returns True after the successful completeion of events.
+             
+        """
+        buffer = client_socket.recv(self.buffsize)
+
+        if len(buffer) == 0:
+            print("Force exit unknown client.")
+            client_socket.close()
+            return (False, "Error")
+
+        buffer = buffer.decode("utf-8")
+        username = buffer.split(',')[0]
+
+        ts = self.getTimeStamp(buffer)
+        self.incrementTimeStamp(ts)
+
+        self.incrementTimeStamp(self.timestamp)  # for send
+        ts = ', <{timestamp}>.'.format(timestamp=self.timestamp)
+
+        if self.clients.get(username) is None:
+            self.clients[username] = client_socket
+            client_socket.sendall(bytes("Accepted" + ts, 'utf-8'))
+            buffer = "{u} joined the messenger room, <{ts}>.".format(
+                u=username, ts=self.timestamp)
+            self.broadcast(username, buffer)
+        else:
+            client_socket.sendall(bytes("Rejected" + ts, 'utf-8'))
+            self.registerNewUser(client_socket)
+
+        return (True, username)
+
+    def handleQuit(self, username):
+        """
+            This function handles the client quit event.
+        Args:    
+            username: The client's username who wants to quit. 
+
+        """
+        buffer = "{u} left the messenger room, <{ts}>".format(
+            u=username, ts=self.timestamp)
+        self.broadcast(username, buffer)
+        client_socket = None
+
+        with self.lock_clients:
+            client_socket = self.clients[username]
+            self.clients.pop(username, None)
+
+        print(">> " + username + " exited")
+        client_socket.close()
+
+    def broadcast(self, username, buffer):
+        """
+            This function broadcast takes the buffer and broadcast to all the connected clients.
+        Args:    
+            username: This is client username of type string.
+            buffer: buffer of type string to transmit. 
+             
+        """
+        for key in self.clients.keys():
+            self.incrementTimeStamp(self.timestamp)
+            buffer = buffer.split(",")[0]
+            buffer = buffer + ",<{ts}> ".format(ts=self.timestamp)
+            self.clients[key].sendall(bytes(buffer, 'utf-8'))
+
+        print(">> Broadcast: ", buffer)
+
+    def getTimeStamp(self, message):
+        """
+        This method will extract the timestamp from message and returns it as integer.
+        Args:
+            message: Its the message of type string.
+        
+        Returns: 
+            It will return the timestamp of type int.
+        """
+        message = message.split('<')[1]
+        message = message.split('>')[0]
+        return int(message)
+
+    def incrementTimeStamp(self, timestamp):
+        """
+        This method is used to increment the timestamp according to lamport's algorithm.
+        
+        Args:    
+            timestamp: timestamp to comapare.
+        
+        Returns: 
+            updated timestamp 
+        """
+        temp = 0
+        with self.lock_timestamp:
+            self.timestamp = max(self.timestamp, timestamp) + 1
+            temp = self.timestamp
+        return temp
+
+if __name__ == "__main__":
+    IP = "localhost"
+    port = 4444
+    server = Server(IP, port)
+    server.listen()
+    exit()
