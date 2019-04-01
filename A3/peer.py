88c418c0a1e4d87e8129206ee55af134885f7670
Waris Gill <wgill18@ku.edu.tr>
2019-04-02 02:19:44 +0300

    finger table is done

--- A3/peer.py
+++ A3/peer.py
@@ -18,8 +18,9 @@ class Peer(object):
     def __init__(self,IP,PORT):
         self.IP = IP
         self.PORT = PORT
+        self.num_bits = 6
         self.ID = self.dhtHash(self.IP + ":" + self.PORT)
-        self.FT = [None] * 5 # change it later
+        self.FT = [None] * 7 # change it later
         self.NOTES_DICTIONARY = {}
         self.successor_id = -1
         self.successor_peer = None
@@ -29,9 +30,8 @@ class Peer(object):
         print("> Own ID: {0}".format(self.ID))
 
     def dhtHash(self,s):
-        num_bits = 6
         h = md5(s.encode())  
-        key = int(h.hexdigest(),16) % (2**num_bits)
+        key = int(h.hexdigest(),16) % (2**self.num_bits)
         return key
 
     def connect(self,ip,port):
@@ -40,54 +40,36 @@ class Peer(object):
 
     def lookup(self,key):
         # add condition to check the keys values.
-        print("(Lookup at node :{0} for key {1}.)".format(self.ID,key))
+        # print("\n(Lookup at node :{0} for key {1}.)".format(self.ID,key))
         temp = "<-N:{0}".format(self.ID)     
         if self.successor_id == -1 and self.predecssor_id == -1: # only 1 node in the system.
-            print("(Lookup ended. 1)\n")
-            return (self.IP, self.PORT,temp)
+            print(">> I am responsible for key {0}".format(key))
+            # print("(Lookup ended. 1)\n")
+            return (self.IP, self.PORT,temp,self.ID)
         elif key > self.predecssor_id and key  <= self.ID:
-            print("(Lookup ended. 2)\n")
-            return (self.IP, self.PORT,temp)
+            print(">> I am responsible for key {0}".format(key))
+            # print("(Lookup ended. 2)\n")
+            return (self.IP, self.PORT,temp,self.ID)
         elif key > self.predecssor_id and self.predecssor_id > self.ID:
-            print("(Lookup ended. 3)\n")                           
-            return (self.IP, self.PORT,temp)
+            print(">> I am responsible for key {0}".format(key))
+            # print("(Lookup ended. 3)\n")                           
+            return (self.IP, self.PORT,temp,self.ID)
         elif key <= self.ID and self.predecssor_id > self.ID:
-            print("(Lookup ended. 4)\n")
-            return (self.IP, self.PORT,temp)    
+            print(">> I am responsible for key {0}".format(key))
+            # print("(Lookup ended. 4)\n")
+            return (self.IP, self.PORT,temp,self.ID)    
         else:
-            print("(Lookup .... 5)")
+            # print("(Lookup .... 5)")
             peer = self.findBestFTEntry(key)
-            ip,port,path  =   peer.lookup(key)
-            print("(Lookup ended. 5)\n")            
-            return (ip,port,temp+path)
+            ip,port,path,temp_id  =   peer.lookup(key)
+            # print("(Lookup ended. 5)\n")            
+            return (ip,port,temp+path,temp_id)
             
-       
-
-     
-
     def findBestFTEntry(self,key):
         return self.successor_peer
     
     def getID(self):
         return self.ID
-
-    def join(self):
-        return None
-    
-    def post(self):
-        return None
-    
-    def get(self):
-        return None
-    
-    def leave(self):
-        return None
-    
-    def updateFingerTable(self):
-        return None
-
-    def displayState(self):
-        return None
 
     def getPredID(self):
         return self.predecssor_id
@@ -99,21 +81,32 @@ class Peer(object):
         self.predecssor_id = id 
         self.predecssor_peer = self.connect(ip,port) #Pyro4.Proxy(uri)
         self.pred_addr = (self.predecssor_id,ip,port)
-        print("(Pred Update: Own ID = {0}, Pred ID = {1}, Succ ID: {2})".format(self.ID,self.predecssor_id,self.successor_id))  
-    
+        print("(Pred Update: Pred ID = {0}, Own ID = {1}, Succ ID: {2})".format(self.predecssor_id, self.ID, self.successor_id))  
+
     def setSucc(self,id,ip,port):
         self.successor_id = id
         self.successor_peer = self.connect(ip,port)  #Pyro4.Proxy(uri)
-        self.FT[0] = self.successor_peer
-        print("(Succ Update: Own ID = {0}, Pred ID = {1}, Succ ID: {2})".format(self.ID,self.predecssor_id,self.successor_id))
+        self.FT[0] = (ip,port,id)
+        print("(Succ Update: Pred ID = {0}, Own ID = {1}, Succ ID: {2})".format(self.predecssor_id, self.ID, self.successor_id))
+    
+    def updateFingerTable(self):
+        keys = []
+        for i in range(0,len(self.FT)):
+            temp = (self.ID + 2**i ) % (2**self.num_bits)
+            keys.append(temp)
+            ip,port,path,temp_id = self.lookup((self.ID + 2**i ) % (2**self.num_bits))
+            self.FT[i] = (ip,port,temp_id)
+        
+        print("Keys : {0}".format(keys))
+        print("Finger Table of Node {0}".format(self.ID))
+        for i in range(0, len(self.FT)):
+            print("{0} | {1}".format(i,self.FT[i][2]))
 
     def joinSetup(self,ip,port):
         # connecting to the correct successor in chord    
         self.successor_peer = self.connect(ip,port) 
         self.successor_id = self.successor_peer.getID()
-        self.FT[0] = self.successor_peer
-
-
+        self.FT[0] = (ip,port,self.successor_id)
         if self.successor_peer.getPredID() == -1: # if there is only 1 peer in the chord
             # setting connected node pred and succ
             self.successor_peer.setPred(self.ID,self.IP,self.PORT)
@@ -122,6 +115,7 @@ class Peer(object):
             self.predecssor_id = self.successor_id
             self.predecssor_peer= self.connect(ip,port) 
             self.pred_addr = (self.predecssor_id,ip,port)
+            
         else:
             # 1. Get ip and port of succ pred. 
             # 2  Update its pred to yourself. 
@@ -132,7 +126,7 @@ class Peer(object):
             id,ip,port = self.successor_peer.getPredAddress()             
             
             # step 2
-            self.successor_peer.setPred(self.ID,self.IP,self.PORT) 
+            self.successor_peer.setPred(self.ID,self.IP,self.PORT)
             # step 3
             self.predecssor_id = id
             self.predecssor_peer = self.connect(ip,port)
@@ -144,41 +138,59 @@ class Peer(object):
 
     
     # ============================== Client Handling ==================
-    def handleJoin(self):
+    def handleJoin(self,ip,port):
         # ip = input("Enter friend's IP")
-        ip = "localhost"
-        port = input("Enter friends port: ")
         bootstrap_peer = self.connect(ip,port) #Pyro4.Proxy(uri)
-
-        ip,port,path = bootstrap_peer.lookup(self.ID) # (disconnect not handled)
+        ip,port,path,temp_id = bootstrap_peer.lookup(self.ID) # (disconnect not handled)
         print(">Lookup Path:  " + path)
         self.joinSetup(ip,port)
         print(">Joining Completed.")
 
     
-    def menu(self):
+    def menu(self,connect_port):
+        ip = "localhost"
+        if connect_port != "None":
+            self.handleJoin(ip,connect_port)
         while True:
             print("\n*************************** Chord Menu  ****************************")
             print("1. Please enter 1 to join the Chord.")
-            print("2. Please enter 2 to leave the Chord.")
-            print("3. Please enter 3 to add a note.")
-            print("4. Please enter 4 to retrieve a note.")
+            # print("2. Please enter 2 to leave the Chord.")
+            # print("3. Please enter 3 to add a note.")
+            # print("4. Please enter 4 to retrieve a note.")
+            print("5. Update Finger Table.")
+            print("6. Lookup Key.")
+
             n = input(">")
             
             if n == "1":
-                self.handleJoin()
+                port = input("Enter friends port: ")
+                self.handleJoin(ip,port)
+            elif n == "5":
+                self.updateFingerTable()
+            elif n == "6":
+                key = input("Enter key: ")
+                result =self.lookup(int(key))
+                print("Result : {0}".format(result))
             else:
                 print(">Error: Please select correct option.")
             print("======================================================================")
         
 def main():    
     ip = "localhost"
-    port =input("Enter PORT:")
+    if len(sys.argv)==1:
+        port =input("Enter PORT:")
     
+    port = sys.argv[1]
     # port = int(port)
     PEER = Peer(ip,port)
-      
-    t = threading.Thread(target=PEER.menu, args=())
+
+    temp_port = None
+    if len(sys.argv) < 3:
+        temp_port = "None"
+    else:
+        temp_port = sys.argv[2]
+
+    t = threading.Thread(target=PEER.menu, args=(temp_port,))
     t.start()
 
     Pyro4.Daemon.serveSimple({
